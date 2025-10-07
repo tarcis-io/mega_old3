0ecba0f0d0133e0b316bd697a6e2d27dc1f1ea07
Tarcisio Oliveira <t@rcis.io>
2025-10-07 19:25:05 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,4 @@
+// Package config loads, parses, and provides the application configuration.
 package config
 
 import (
@@ -5,17 +6,30 @@ import (
 )
 
 type (
+	// Config
 	Config struct {
+		// ServerAddress
+		// Default: "localhost:8080".
 		ServerAddress string
 
+		// ServerReadTimeout
+		// Default: 5 * time.Second.
 		ServerReadTimeout time.Duration
 
+		// ServerReadHeaderTimeout
+		// Default: 2 * time.Second.
 		ServerReadHeaderTimeout time.Duration
 
+		// ServerWriteTimeout
+		// Default: 10 * time.Second.
 		ServerWriteTimeout time.Duration
 
+		// ServerIdleTimeout
+		// Default: 60 * time.Second.
 		ServerIdleTimeout time.Duration
 
+		// ServerShutdownTimeout
+		// Default: 15 * time.Second.
 		ServerShutdownTimeout time.Duration
 	}
 )
