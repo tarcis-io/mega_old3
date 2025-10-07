c0f303e3246c8c5c79ca32deefb70f785fd7c35d
Tarcisio Oliveira <t@rcis.io>
2025-10-07 19:17:29 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,21 @@
 package config
 
+import (
+	"time"
+)
+
 type (
 	Config struct {
+		ServerAddress string
+
+		ServerReadTimeout time.Duration
+
+		ServerReadHeaderTimeout time.Duration
+
+		ServerWriteTimeout time.Duration
+
+		ServerIdleTimeout time.Duration
+
+		ServerShutdownTimeout time.Duration
 	}
 )
