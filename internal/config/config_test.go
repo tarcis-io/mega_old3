66adde82ea0db9c3658922c79267b26d69a8dc20
Tarcisio Oliveira <t@rcis.io>
2025-10-11 01:21:44 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -15,6 +15,14 @@ type (
 )
 
 func TestNew(t *testing.T) {
+	testCases := []testCase{}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			for envKey, envValue := range tc.envValues {
+				t.Setenv(envKey, envValue)
+			}
+		})
+	}
 }
 
 func mustParseDuration(duration string) time.Duration {
