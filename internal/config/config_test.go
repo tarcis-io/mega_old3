207fe5d84048828e08b2770a7210e9f0c9316be5
Tarcisio Oliveira <t@rcis.io>
2025-10-11 01:00:01 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -2,7 +2,16 @@ package config
 
 import (
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
+}
+
+func mustParseDuration(duration string) time.Duration {
+	timeDuration, err := time.ParseDuration(duration)
+	if err != nil {
+		panic(err)
+	}
+	return timeDuration
 }
