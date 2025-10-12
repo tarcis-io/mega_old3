5364e2cb4b8f48eab28e248b715b811f3eee2c1a
Tarcisio Oliveira <t@rcis.io>
2025-10-12 18:56:28 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,24 @@
 package server
 
+import (
+	"net/http"
+	"time"
+)
+
 type (
 	Server struct {
+		address string
+
+		router *http.ServeMux
+
+		readTimeout time.Duration
+
+		readHeaderTimeout time.Duration
+
+		writeTimeout time.Duration
+
+		idleTimeout time.Duration
+
+		shutdownTimeout time.Duration
 	}
 )
