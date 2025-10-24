bc5df7de3690218d850b9d72bf7487843f07eb6d
Tarcisio Oliveira <t@rcis.io>
2025-10-24 20:44:03 +0000

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -1,23 +1,4 @@
-// Package main is the entry point for the mega application.
 package main
 
-import (
-	"log/slog"
-	"os"
-
-	"mega/internal/config"
-)
-
 func main() {
-	config, err := config.New()
-	if err != nil {
-		panic(err)
-	}
-	if err := run(config, nil); err != nil {
-		os.Exit(1)
-	}
-}
-
-func run(config *config.Config, logger *slog.Logger) error {
-	return nil
 }
