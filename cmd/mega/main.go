a9c1af93ff29fe886057cc5140f5f5d2d7d5fd7e
Tarcisio Oliveira <t@rcis.io>
2025-10-24 22:33:01 +0000

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"log/slog"
+	"os"
 )
 
 func main() {
 	slog.Info("Running application")
+	if err := run(); err != nil {
+		slog.Error("Application stopped unexpectedly", "error", err)
+		os.Exit(1)
+	}
 	slog.Info("Application stopped successfully")
+}
+
+func run() error {
+	return nil
 }
