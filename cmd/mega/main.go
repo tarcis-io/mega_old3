90da43418f1d41c9e32759120e2ffa2b94368a3c
Tarcisio Oliveira <t@rcis.io>
2025-10-25 14:43:36 +0000

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -19,6 +19,16 @@ func main() {
 		log.Fatalf("failed to create logger: %v", err)
 	}
 	slog.SetDefault(logger)
+	slog.Info("Running application")
+	if err := run(config, logger); err != nil {
+		slog.Error("Application stopped unexpectedly", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Application stopped successfully")
+}
+
+func run(config *config.Config, logger *slog.Logger) error {
+	return nil
 }
 
 func newLogger(cfg *config.Config) (*slog.Logger, error) {
