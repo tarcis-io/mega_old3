4869ee402b2ee8f2871bfef8173f5bfa7e3bfd67
Tarcisio Oliveira <t@rcis.io>
2025-10-25 17:15:29 +0000

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "application stopped unexpectedly: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	return nil
 }
