a5dee792a6a777a47c844418fd03c3a60901f748
Tarcisio Oliveira <t@rcis.io>
2025-10-25 13:44:52 +0000

    update

--- cmd/mega/main.go
+++ cmd/mega/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+	"log/slog"
 
 	"mega/internal/config"
 )
 
 func main() {
-	_, err := config.New()
+	config, err := config.New()
 	if err != nil {
 		log.Fatalf("failed to create config: %v", err)
 	}
+	logger, err := newLogger(config)
+	if err != nil {
+		log.Fatalf("failed to create logger: %v", err)
+	}
+	slog.SetDefault(logger)
+}
+
+func newLogger(cfg *config.Config) (*slog.Logger, error) {
+	return nil, nil
 }
