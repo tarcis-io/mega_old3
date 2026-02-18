ad6019b70b62d901e2ca65731a69bb093cb5ba92
Tarcisio Oliveira <t@rcis.io>
2026-02-18 14:04:08 +0000

    update

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,17 @@
+<script lang="ts" setup>
+	const title = ref('Mega');
+</script>
+
 <template>
-	<h1>Hello World, Mega!</h1>
+	<Head>
+		<Meta charset="UTF-8" />
+		<Meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<link rel="icon" href="/favicon.ico" sizes="32x32" />
+		<Link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+		<Link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+		<Title>{{ title }}</Title>
+	</Head>
+	<Body>
+		<h1>Hello World, {{ title }}!</h1>
+	</Body>
 </template>
