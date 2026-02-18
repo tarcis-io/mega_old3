5f04f6f2813a3516487ba3dace8833baf4eb5d41
Tarcisio Oliveira <t@rcis.io>
2026-02-18 17:04:28 +0000

    update

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+export default defineNuxtConfig({
+	app: {
+		head: {
+			charset: 'UTF-8',
+			viewport: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no',
+			link: [
+				{
+					rel: 'icon',
+					href: '/favicon.ico',
+					sizes: '32x32',
+				},
+				{
+					rel: 'icon',
+					href: '/favicon.svg',
+					type: 'image/svg+xml',
+				},
+				{
+					rel: 'apple-touch-icon',
+					href: '/apple-touch-icon.png',
+				},
+				{
+					rel: 'manifest',
+					href: '/manifest.json',
+				},
+			],
+		},
+	},
+	modules: [
+		'@nuxt/ui',
+	],
+});
